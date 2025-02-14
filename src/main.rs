07d09426bf0ed3dd131202bc9255f07bbffa8481
NineLord <eightlord@gmail.com>
2025-02-14 20:08:32 +0200

    testing git2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+#![deny(unused_must_use)]
+
+use std::path::{Path, PathBuf};
+use git2::{Repository, Worktree};
+use anyhow::Result;
+
+const CWD: &str = "/mnt/d/Users/Shaked/MyCleanSpace/Documents/000 VMware/Rust/what2do";
+
+fn main() -> Result<()> {
+	let mut repo = Repository::open(Path::new(CWD))?;
+	for x in repo.blame_file(Path::new("src/main.rs"), None)?.iter() {
+		println!("{}", x.lines_in_hunk());
+	}
+	Ok(())
 }
+// todo
