fe59ad9e438991fef06cfcb22d0e5a9628cf90b4
NineLord <eightlord@gmail.com>
2025-02-14 21:47:25 +0200

    Basic recursive call over all committed files

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,58 @@
 #![allow(unused)]
 #![deny(unused_must_use)]
 
-use std::path::{Path, PathBuf};
-use git2::{Repository, Worktree};
+use std::{
+	fs,
+	path::{Path, PathBuf},
+};
+use git2::{Repository, Blame, ErrorCode};
 use anyhow::Result;
 
-const CWD: &str = "/mnt/d/Users/Shaked/MyCleanSpace/Documents/000 VMware/Rust/what2do";
+fn handle_blame(absolute_path: &Path, relative_path: &Path, repo: &Repository, blame: Blame<'_>) {
+	dbg!(&relative_path);
+}
+
+fn iter_recursively_all_files(absolute_path: PathBuf, mut relative_path: PathBuf, repo: &Repository) -> Result<()> {
+	for entry in fs::read_dir(absolute_path)? {
+		let entry = entry?;
+		let file_type = entry.file_type()?;
+		if file_type.is_file() {
+			relative_path.push(Path::new(&entry.file_name()));
+			match repo.blame_file(&relative_path, None) {
+				Ok(blame) => handle_blame(&entry.path(), &relative_path, repo, blame),
+				Err(error) => match error.code() {
+					ErrorCode::NotFound => (), // New file that wasn't committed
+					_ => unreachable!()
+				},
+			}
+			relative_path.pop();
+		} else if file_type.is_dir() {
+			relative_path.push(Path::new(&entry.file_name()));
+			match repo.blame_file(&relative_path, None) {
+				Err(error) => {
+					match error.code() {
+						ErrorCode::InvalidSpec => // Can't blame directory but it is committed, so need to check it recursively.
+							iter_recursively_all_files(entry.path(), relative_path.clone(), repo)?,
+						ErrorCode::NotFound => (), // The directory wasn't committed, no need to check it recursively.
+						_ => unreachable!()
+					}
+				},
+				Ok(_) => unreachable!("Can't blame directory"),
+			}
+			relative_path.pop();
+		}
+	}
+	Ok(())
+}
 
 fn main() -> Result<()> {
-	let mut repo = Repository::open(Path::new(CWD))?;
-	for x in repo.blame_file(Path::new("src/main.rs"), None)?.iter() {
-		println!("{}", x.lines_in_hunk());
-	}
+	let cwd = std::env::current_dir()?;
+	let repo = Repository::open(&cwd)?;
+	iter_recursively_all_files(cwd, Path::new("").to_path_buf(), &repo)?;
+	// for x in repo.blame_file(Path::new("src/main.rs"), None)?.iter() {
+	// 	println!("{:?}", x.final_signature().when());
+	// }
+	// println!("{}", repo.blame_file(Path::new("src/main.rs"), None)?.get_line(17).unwrap().lines_in_hunk());
 	Ok(())
 }
 // todo
